b499fbfd1875d7b0a992b6ad9950488cf7852c6c
HONGTAO ZHAO <32073310+bruttozz@users.noreply.github.com>
2018-04-26 16:04:18 -0400

    Add files via upload
    phase3.py is the program.
    You can change the account names in the screen_names.
    The other files are the labels of the pictures which are in the specific account.

---
+++ MongoDB/Phase3/phase3.py
@@ -0,0 +1,142 @@
+import tweepy
+import json
+import ffmpeg
+import wget
+import urllib.request
+import io
+import os
+import re
+import requests
+import subprocess
+import sys
+import glob
+from PIL import Image
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+from pymongo import MongoClient
+import pprint
+import bson
+
+# Twitter API credentials
+consumer_key = 'mwzfDqMTCwRxBrGtJnWiBq0Ir'
+consumer_secret = 'PbmHcqAHpoAF5NqT4MjdhLCKOQXdXRYt6v6IFltypENsInrGsd'
+access_key = '920695116150525953-Uf1blZlllGja7CDAHPQrVD5ZriAZNMb'
+access_secret = 'u0WN80vQtLhxT1JfOCd4GU7fXBTolwfxkzzyl1kxlPtF6'
+
+
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+
+# Reference:
+    # https://miguelmalvarez.com/2015/03/03/download-the-pictures-from-a-twitter-feed-using-python/
+def call_api(screen_name, videoname, lable_output):
+    # authorize twitter, initialize tweepy
+    Image_Number = 10
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    # Get the tweets from a user up to 200
+    tweets = api.user_timeline(screen_name = screen_name,
+                               count = 200, include_rts = False,
+                               exclude_replies = True)
+
+    last_id = tweets[-1].id
+
+    while (True):
+        more_tweets = api.user_timeline(screen_name = screen_name,
+                                       count = 200,
+                                       include_rts = False,
+                                       exclude_replies = True,
+                                       max_id = last_id - 1)
+        # If there are no more tweets
+        if (len(more_tweets) == 0):
+            break
+        else:
+            last_id = more_tweets[-1].id - 1
+            tweets = tweets + more_tweets
+
+    # Obtain the full path for the images
+    media_files = set()
+    for status in tweets:
+        media = status.entities.get('media', [])
+        if(len(media) > 0):
+            media_files.add(media[0]['media_url'])
+
+    count = 0
+
+    for media_file in media_files:
+        #print(media_file)
+        wget.download(media_file,'image'+str(count)+media_file[-4:])
+
+        count += 1
+        if count == Image_Number:
+            break
+
+    # Instantiates a client, specify the cert file
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/usr/local/lib/googlecloudsdk/EC500hw1-70f8d0b192a0.json"
+    client = vision.ImageAnnotatorClient()
+
+    data = {}
+    data['Pictures'] = []
+    data['Account'] = screen_name
+    pictures = [pic for pic in os.listdir(".") if pic.endswith('jpg')]
+    picNum = 0
+    order = 0
+    for i in pictures:
+
+        file = os.path.join(os.path.dirname(__file__),i)
+        new = str(picNum) +'.jpg'
+        os.renames(file, new)
+        Subject = {}
+        picNum = picNum + 1
+
+        # Loads the image into memory
+        with io.open(new, 'rb') as image_file:
+             content = image_file.read()
+
+        image = types.Image(content=content)
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+
+        label_list = []
+        for label in labels:
+           label_list.append(label.description)
+
+        Subject[str(order)] =  label_list
+        data['Pictures'].append(Subject)
+
+        order += 1
+
+    with open(lable_output,'w') as JSONObject:
+        json.dump(data, JSONObject, indent = 4, sort_keys = True)
+    client = MongoClient()
+    db = client.picture.database
+    collection = db.picture_collection
+    db.posts.insert(data)
+
+    pprint.pprint(db.posts.find_one({'Account':screen_name}))
+
+    #os.system("cat *.jpg | ffmpeg -f image2pipe -framerate .5 -i - -vf 'crop=in_w-1:in_h' -vcodec libx264 video.mp4")
+
+if __name__ == '__main__':
+
+    screen_names = ['katyperry','NBA','ManUtd','justinbieber','taylorswift13','ladygaga','TheEllenShow',
+                    'Cristiano','YouTube','jtimberlake','KimKardashian','britneyspears','ArianaGrande',
+                    'ddlovato','selenagomez','cnnbrk','shakira','jimmyfallon','BillGates']
+    for screen_name in screen_names:
+	       call_api(screen_name, screen_name, screen_name)
+
+    while(True):
+    	try:
+    		for n in range(20):
+    			os.remove(str(n) + ".jpg")
+    	except:
+    		break
